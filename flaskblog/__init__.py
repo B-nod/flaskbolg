a7a2e567b375cbf4c833928b7134e655242c23b3
B-nod <binodtamang245@gmail.com>
2024-02-20 22:04:05 +0545

    update profile

---
+++ flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= 'b92d65e1cbdb8a13c031dc5ad9f91234'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+app.app_context().push()
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
